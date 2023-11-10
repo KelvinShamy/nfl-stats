e0642870c49d2cd6f3a95b5d8c7a9cf9f769ebee
“KelvinShamy” <kelvinshamy@gmail.com>
2023-11-10 01:17:52 -0700

    implement simple static data

--- src/components/TeamDisplay.jsx
+++ src/components/TeamDisplay.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import styles from '../App.scss'
 
 export default function TeamDisplay({data}) {
     return (
         <div>
             <h2>{data.Standings.Tm}</h2>
-            <div className={styles.flexRow}>
-                <div>W: {data.Standings.W}</div>
-                <div>L: {data.Standings.L}</div>
+            <div>
+                <h3>Record: {data.Standings.W}-{data.Standings.L}</h3>
+                <h4>Offense</h4>
+                <div>Yds/Game: {data["Offense Per Game"].Yds}</div>
+                <div>Passing/Game: {data["Offense Per Game"].Passing.Yds}</div>
+                <div>Rushing/Game: {data["Offense Per Game"].Rushing.Yds}</div>
+                <h4>Defense</h4>
+                <div>Yds/Game: {data["Defense Per Game"].Yds}</div>
+                <div>Passing/Game: {data["Defense Per Game"].Passing.Yds}</div>
+                <div>Rushing/Game: {data["Defense Per Game"].Rushing.Yds}</div>
             </div>
         </div>
     )
